ccb18a84aaeffc000edd7a513595a7ca916a99f2
Egor Pugin <egor.pugin@gmail.com>
2018-12-19 03:15:51 +0300

    Add missing file.

---
+++ src/manager/pch.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "cppan_version.h"
+#include "lock.h"
+#include "property_tree.h"
+#include "settings.h"
+#include "dependency.h"
+#include "remote.h"
+
+#include <exceptions.h>
+#include <filesystem.h>
+#include <../support/hash.h>
+#include <http.h>
+
+#include <boost/algorithm/string.hpp>
+#include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
+#include <primitives/command.h>
+#include <primitives/date_time.h>
+#include <primitives/log.h>
+#include <primitives/yaml.h>
+#include <primitives/stdcompat/variant.h>
+#include <primitives/stdcompat/optional.h>
+#include <sqlite3.h>
+
+#include <primitives/command.h>
+#include <primitives/exceptions.h>
+#include <primitives/executor.h>
+#include <primitives/filesystem.h>
+#include <primitives/overload.h>
+#include <primitives/pack.h>
+
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+
+#include <primitives/sw/settings.h>
+
+#include <boost/algorithm/string.hpp>
+
+#include <primitives/executor.h>
+#include <primitives/hash.h>
+#include <primitives/hasher.h>
+#include <primitives/pack.h>
+#include <primitives/templates.h>
+#include <primitives/win32helpers.h>
+#include <primitives/sw/settings.h>
+#include <nlohmann/json.hpp>
+
+#include <fmt/ostream.h>
+#include <primitives/templates.h>
+#include <grpcpp/grpcpp.h>
+#include <boost/dll.hpp>
+
+#include <primitives/filesystem.h>
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <shared_mutex>
+
+#include <set>
+#include <map>
+#include <string>
+
+#include <bitset>
+
+#include <primitives/date_time.h>
+#include <primitives/stdcompat/optional.h>
+#include <sqlpp11/sqlite3/connection.h>
+
+#include <chrono>
+#include <memory>
+#include <vector>
+
+#include <primitives/db/sqlite3.h>
+#include <sqlite3.h>
+#include <sqlpp11/custom_query.h>
+#include <sqlpp11/sqlite3/sqlite3.h>
+#include <sqlpp11/sqlpp11.h>
+#include <db_service.h>
+#include <db_packages.h>
+#include "sqlite_database.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <grpc_helpers.h>
+
+#include <primitives/templates.h>
+
+#include <grpcpp/grpcpp.h>
+#include <nlohmann/json.hpp>
+
+#include <chrono>
+#include <memory>
+#include <vector>
