5b8fa63ff5157edcce003ee3691a60ac85630b3e
Egor Pugin <egor.pugin@gmail.com>
2019-01-17 23:03:08 +0300

    Implement self upgrade.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -32,6 +32,7 @@
 #include <primitives/executor.h>
 #include <primitives/file_monitor.h>
 #include <primitives/lock.h>
+#include <primitives/pack.h>
 #include <primitives/sw/settings.h>
 #include <primitives/sw/main.h>
 #include <primitives/thread.h>
@@ -83,6 +84,8 @@ int main(int argc, char **argv);
 int sw_main(const Strings &args);
 void stop();
 void setup_log(const std::string &log_level);
+void self_upgrade();
+void self_upgrade_copy(const path &dst);
 
 #ifdef _WIN32
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
@@ -110,6 +113,9 @@ extern bool gVerbose;
 static cl::opt<bool> trace("trace", cl::desc("Trace output"));
 static cl::opt<int> jobs("j", cl::desc("Number of jobs"), cl::init(-1));
 
+static cl::opt<bool> cl_self_upgrade("self-upgrade", cl::desc("Upgrade client"));
+static cl::opt<path> cl_self_upgrade_copy("internal-self-upgrade-copy", cl::desc("Upgrade client: copy file"), cl::ReallyHidden);
+
 extern cl::opt<bool> useFileMonitor;
 
 #include <boost/asio/io_context.hpp>
@@ -118,6 +124,18 @@ extern cl::opt<bool> useFileMonitor;
 int setup_main(const Strings &args)
 {
     // some initial stuff
+
+    if (!cl_self_upgrade_copy.empty())
+    {
+        self_upgrade_copy(cl_self_upgrade_copy);
+        return 0;
+    }
+
+    if (cl_self_upgrade)
+    {
+        self_upgrade();
+        return 0;
+    }
 
     if (!working_directory.empty())
     {
@@ -741,6 +759,79 @@ EXPORT_FROM_EXECUTABLE
 std::string getProgramName()
 {
     return PACKAGE_NAME_CLEAN;
+}
+
+void self_upgrade()
+{
+#ifdef _WIN32
+    path client = "/client/sw-master-windows-client.zip";
+#elif __APPLE__
+    path client = "/client/sw-master-macos-client.zip";
+#else
+    path client = "/client/.service/sw-master-linux-client.zip";
+#endif
+
+    auto &s = Settings::get_user_settings();
+
+    std::cout << "Downloading checksum file" << "\n";
+    auto md5sum = boost::algorithm::trim_copy(download_file(s.remotes[0].url + client.u8string() + ".md5"));
+
+    auto fn = fs::temp_directory_path() / (unique_path() += client.extension());
+    std::cout << "Downloading the latest client" << "\n";
+    download_file(s.remotes[0].url + client.u8string(), fn, 50_MB);
+    if (md5sum != md5(fn))
+        throw std::runtime_error("Downloaded bad file (md5 check failed)");
+
+    std::cout << "Unpacking" << "\n";
+    auto tmp_dir = fs::temp_directory_path() / "sw.bak";
+    unpack_file(fn, tmp_dir);
+    fs::remove(fn);
+
+    // self update
+    auto program = boost::dll::program_location();
+#ifdef _WIN32
+    auto exe = (tmp_dir / "sw.exe").wstring();
+    auto arg0 = L"\"" + exe + L"\"";
+    auto dst = L"\"" + program.wstring() + L"\"";
+    std::cout << "Replacing client" << "\n";
+    auto cmd_line = arg0 + L" -internal-self-upgrade-copy " + dst;
+    STARTUPINFO si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+    if (!CreateProcess(exe.c_str(), &cmd_line[0], 0, 0, 0, 0, 0, 0, &si, &pi))
+    {
+        throw std::runtime_error("errno = "s + std::to_string(errno) + "\n" +
+            "Cannot do a self upgrade. Replace this file with newer SW client manually.");
+    }
+#else
+    auto cppan = tmp_dir / "sw";
+    fs::permissions(cppan, fs::perms::owner_all | fs::perms::group_exec | fs::perms::others_exec);
+    fs::remove(program);
+    fs::copy_file(cppan, program);
+    fs::remove(cppan);
+#endif
+}
+
+void self_upgrade_copy(const path &dst)
+{
+    int n = 3;
+    while (n--)
+    {
+        std::cout << "Waiting old program to exit...\n";
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        try
+        {
+            fs::copy_file(boost::dll::program_location().wstring(), dst, fs::copy_options::overwrite_existing);
+            break;
+        }
+        catch (std::exception &e)
+        {
+            std::cerr << "Cannot replace program with new executable: " << e.what() << "\n";
+            if (n == 0)
+                throw;
+            std::cerr << "Retrying... (" << n << ")\n";
+        }
+    }
+    std::cout << "Success!\n";
 }
 
 #if _MSC_VER
