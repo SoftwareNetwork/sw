e422da69f9e31d93d6c806d24205c6f2565eba43
Egor Pugin <egor.pugin@gmail.com>
2019-08-12 12:32:29 +0300

    Do not use rsp for ar.

--- src/sw/driver/compiler/compiler.cpp
+++ src/sw/driver/compiler/compiler.cpp
@@ -802,7 +802,7 @@ void GNULibrarian::prepareCommand1(const Target &t)
     // TODO: investigate, fix and remove?
     cmd->remove_outputs_before_execution = true;
 
-    if (t.getBuildSettings().TargetOS.isApple())
+    if (t.getSolution().getHostOs().isApple() || t.getBuildSettings().TargetOS.isApple())
         cmd->use_response_files = false;
 
     // can be zero imput files actually: lib.exe /DEF:my.def /OUT:x.lib
