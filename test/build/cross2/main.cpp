0945037b1064aae108b4c4459a3cbc6123957c73
Egor Pugin <egor.pugin@gmail.com>
2019-11-28 17:52:17 +0300

    Improve cross2 test.

--- test/build/cross2/main.cpp
+++ test/build/cross2/main.cpp
@@ -2,14 +2,43 @@
 
 #include <iostream>
 
+void print(const char *s)
+{
+    //printf("\n%s\n\n", s); // works
+    std::cout << "\n" << s << "\n\n"; // has strong impact, does not work with lld
+    //try { throw 1; } catch (...) {}
+}
+
 void f()
 {
+    try { throw std::runtime_error(""); }
+    catch (std::exception&) { print("std::exception&"); }
+
+    try { throw std::runtime_error(""); }
+    catch (std::exception) { print("std::exception"); }
+
+
+
+
+
+
     try { throw 5; }
-    catch (int) { std::cout << "int\n"; }
+    catch (int) { print("int"); }
+
     try { throw sw::RuntimeError(""); }
-    catch (sw::RuntimeError) { std::cout << "sw::RuntimeError\n"; }
+    catch (sw::RuntimeError &) { print("sw::RuntimeError&"); }
+
     try { throw sw::RuntimeError(""); }
-    catch (std::exception&) { std::cout << "std::exception\n"; }
+    catch (sw::RuntimeError) { print("sw::RuntimeError"); }
+
+    try { throw sw::RuntimeError(""); }
+    catch (std::exception&) { print("sw::RuntimeError std::exception&"); }
+
+    try { throw std::runtime_error(""); }
+    catch (std::exception) { print("std::exception"); }
+
+    try { throw std::runtime_error(""); }
+    catch (std::exception&) { print("std::exception&"); }
 }
 
 int main()
