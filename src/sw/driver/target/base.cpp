109d5d99174add6775899c9ff2e4798d9f20f992
Egor Pugin <egor.pugin@gmail.com>
2019-12-22 01:23:07 +0300

    Use dry-run = false by default.

--- src/sw/driver/target/base.cpp
+++ src/sw/driver/target/base.cpp
@@ -230,7 +230,7 @@ void TargetBase::setupTarget(TargetBaseType *t) const
         t->source = t0->source ? t0->source->clone() : nullptr;
 
     t->IsConfig = IsConfig; // TODO: inherit from reconsider
-    t->DryRun = DryRun; // TODO: inherit from reconsider
+    //t->DryRun = DryRun; // TODO: inherit from reconsider
 
     t->main_build_ = main_build_; // ok, take from here (this, parent)
     t->command_storage = command_storage; // ok, take from here (this, parent)
