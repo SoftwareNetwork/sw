90f464b15875101b59e96b245f3f600453e73c26
Egor Pugin <egor.pugin@gmail.com>
2020-03-16 11:37:04 +0300

    Move more code to GPLv3+.

--- src/sw/client/cli/client.cpp
+++ src/sw/client/cli/client.cpp
@@ -1,6 +1,6 @@
 /*
  * SW - Build System and Package Manager
- * Copyright (C) 2017-2019 Egor Pugin
+ * Copyright (C) 2017-2020 Egor Pugin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
