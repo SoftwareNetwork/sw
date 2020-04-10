31df32f6c7df2f54793079c314e7cd4e567dca8a
Egor Pugin <egor.pugin@gmail.com>
2020-04-10 15:15:47 +0300

    More multiconf tests.

--- test/build/cpp/multiconf/multiconf.bat
+++ test/build/cpp/multiconf/multiconf.bat
@@ -1,5 +1,6 @@
 sw ^
     %* ^
+    -sfc ^
     -configuration Debug,ReleaseWithDebInfo,MinSizeRel,Release ^
     -platform win32,win64,arm,arm64 ^
     -compiler msvc ^
@@ -7,5 +8,36 @@ sw ^
     -mt -md ^
     build
 
-:: clangcl not working atm (linker errors)
-:: -compiler msvc,clangcl
+:: clangcl does not work for arm
+
+sw ^
+    %* ^
+    -sfc ^
+    -configuration Debug,ReleaseWithDebInfo,MinSizeRel,Release ^
+    -platform win32,win64 ^
+    -compiler clangcl ^
+    -static -shared ^
+    -mt -md ^
+    build
+
+sw ^
+    %* ^
+    -sfc ^
+    -configuration Debug,ReleaseWithDebInfo,MinSizeRel,Release ^
+    -platform arm64 ^
+    -compiler clangcl ^
+    -static -shared ^
+    -mt -md ^
+    build
+
+:: clang does not work for x86,arm,aarch64
+
+sw ^
+    %* ^
+    -sfc ^
+    -configuration Debug,ReleaseWithDebInfo,MinSizeRel,Release ^
+    -platform win64 ^
+    -compiler clang ^
+    -static -shared ^
+    -mt -md ^
+    build
