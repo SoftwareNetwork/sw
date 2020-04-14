ad219d6a673839a0b9005e282413221d696844ea
Egor Pugin <egor.pugin@gmail.com>
2020-04-15 01:39:43 +0300

    Build winreg dep only on windows.

--- sw.cpp
+++ sw.cpp
@@ -144,17 +144,20 @@ void build(Solution &s)
         cpp_driver.Public += core,
             "pub.egorpugin.primitives.patch-master"_dep,
             "org.sw.demo.ToruNiina.toml11"_dep,
-            "org.sw.demo.giovannidicanio.winreg"_dep,
             "org.sw.demo.boost.assign"_dep,
             "org.sw.demo.boost.bimap"_dep,
             "org.sw.demo.boost.uuid"_dep;
+        cpp_driver.Public -= "org.sw.demo.giovannidicanio.winreg"_dep;
         cpp_driver += "src/sw/driver/.*"_rr;
         cpp_driver -= "src/sw/driver/misc/delay_load_helper.cpp";
         gen_flex_bison("org.sw.demo.lexxmark.winflexbison"_dep, cpp_driver, "src/sw/driver/bazel/lexer.ll", "src/sw/driver/bazel/grammar.yy");
         if (cpp_driver.getCompilerType() == CompilerType::MSVC || cpp_driver.getCompilerType() == CompilerType::ClangCl)
             cpp_driver.CompileOptions.push_back("-bigobj");
         if (cpp_driver.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            cpp_driver.Public += "org.sw.demo.giovannidicanio.winreg"_dep;
             cpp_driver += "dbghelp.lib"_slib;
+        }
         //else if (s.getBuildSettings().Native.CompilerType == CompilerType::GNU)
             //cpp_driver.CompileOptions.push_back("-Wa,-mbig-obj");
         {
