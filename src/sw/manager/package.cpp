ff7541a4553a51ea472c4e8b0970d04033ad499b
Egor Pugin <egor.pugin@gmail.com>
2020-05-05 00:04:11 +0300

    Remove more duplicate code.

--- src/sw/manager/package.cpp
+++ src/sw/manager/package.cpp
@@ -18,74 +18,11 @@ DECLARE_STATIC_LOGGER(logger, "package");
 namespace sw
 {
 
-static path getHashPathFromHash(const String &h, int nsubdirs, int chars_per_subdir)
-{
-    path p;
-    int i = 0;
-    for (; i < nsubdirs; i++)
-        p /= h.substr(i * chars_per_subdir, chars_per_subdir);
-    p /= h.substr(i * chars_per_subdir);
-    return p;
-}
-
 String getSourceDirectoryName()
 {
     // we cannot change it, because server already has such packages
     // introduce versions to change this or smth
     return "sdir";
-}
-
-Package::Package(const IStorage &storage, const PackageId &id)
-    : storage(storage), PackageId(id)
-{
-}
-
-Package::Package(const Package &rhs)
-    : PackageId(rhs), storage(rhs.storage), data(rhs.data ? rhs.data->clone() : nullptr)
-{
-}
-
-String Package::getHash() const
-{
-    // move these calculations to storage?
-    switch (storage.getSchema().getHashVersion())
-    {
-    case 1:
-        return blake2b_512(getPath().toStringLower() + "-" + getVersion().toString());
-    }
-
-    throw SW_RUNTIME_ERROR("Unknown hash schema version: " + std::to_string(storage.getSchema().getHashVersion()));
-}
-
-path Package::getHashPath() const
-{
-    // move these calculations to storage?
-    switch (storage.getSchema().getHashPathFromHashVersion())
-    {
-    case 1:
-        return ::sw::getHashPathFromHash(getHash(), 4, 2); // remote consistent storage paths
-    case 2:
-        return ::sw::getHashPathFromHash(getHashShort(), 2, 2); // local storage is more relaxed
-    }
-
-    throw SW_RUNTIME_ERROR("Unknown hash path schema version: " + std::to_string(storage.getSchema().getHashPathFromHashVersion()));
-}
-
-String Package::getHashShort() const
-{
-    return shorten_hash(getHash(), 8);
-}
-
-const PackageData &Package::getData() const
-{
-    if (!data)
-        data = std::move(storage.loadData(*this));
-    return *data;
-}
-
-const IStorage &Package::getStorage() const
-{
-    return storage;
 }
 
 LocalPackage::LocalPackage(const LocalStorage &storage, const PackageId &id)
