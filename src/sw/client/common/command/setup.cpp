04846a56aca475f52e6994bba0f3435baaf0cd5c
Egor Pugin <egor.pugin@gmail.com>
2020-05-09 17:13:34 +0300

    [setup] Add --uninstall option to remove sw from the system. Cleanup granularity is controlled by --level option. Closes #38.

--- src/sw/client/common/command/setup.cpp
+++ src/sw/client/common/command/setup.cpp
@@ -18,6 +18,7 @@
 
 #include "../commands.h"
 #include "../inserts.h"
+#include "../sw_context.h"
 
 #include <sw/manager/storage.h>
 
@@ -36,23 +37,51 @@ const String &getCmakeConfig()
     return sw_config_cmake;
 }
 
+static std::wstring get_sw_registry_key()
+{
+    return L"sw";
+}
+
+static std::wstring get_sw_cmake_registry_key()
+{
+    return L"Software\\Kitware\\CMake\\Packages\\SW";
+}
+
+static path get_sw_linux_scheme_handler()
+{
+    return get_home_directory() / ".local/share/applications" / "sw-opener.desktop";
+}
+
+static path get_cmake_dir(const path &root)
+{
+    return root / ".cmake" / "packages";
+}
+
+static String get_cmake_dir_name()
+{
+    return "SW";
+}
+
+static String get_sw_cmake_config_filename()
+{
+    return "SWConfig.cmake";
+}
+
 static void registerCmakePackage(SwClientContext &swctx)
 {
-    const auto sw_cmake_config_filename = "SWConfig.cmake";
-
-    auto write_cmake = [&sw_cmake_config_filename](const path &dir)
+    auto write_cmake = [](const path &dir)
     {
-        auto sw_cmake_dir = dir / ".cmake" / "packages";
-        write_file_if_different(sw_cmake_dir / "SW" / "1", sw_cmake_dir.u8string());
-        write_file_if_different(sw_cmake_dir / sw_cmake_config_filename, getCmakeConfig());
+        auto sw_cmake_dir = get_cmake_dir(dir);
+        write_file_if_different(sw_cmake_dir / get_cmake_dir_name() / "1", sw_cmake_dir.u8string());
+        write_file_if_different(sw_cmake_dir / get_sw_cmake_config_filename(), getCmakeConfig());
     };
 
 #ifdef _WIN32
-    auto dir = swctx.getContext().getLocalStorage().storage_dir_etc / "sw" / "static";
+    auto dir = swctx.getContext(false).getLocalStorage().storage_dir_etc / "sw" / "static";
     // if we write into HKLM, we won't be able to access the pkg file in admins folder
-    winreg::RegKey icon(/*is_elevated() ? HKEY_LOCAL_MACHINE : */HKEY_CURRENT_USER, L"Software\\Kitware\\CMake\\Packages\\SW");
+    winreg::RegKey icon(/*is_elevated() ? HKEY_LOCAL_MACHINE : */HKEY_CURRENT_USER, get_sw_cmake_registry_key());
     icon.SetStringValue(L"", dir.wstring().c_str());
-    write_file_if_different(dir / sw_cmake_config_filename, getCmakeConfig());
+    write_file_if_different(dir / get_sw_cmake_config_filename(), getCmakeConfig());
 
     // cygwin case
     if (auto d = getenv("HOME"))
@@ -62,11 +91,98 @@ static void registerCmakePackage(SwClientContext &swctx)
 #endif
 }
 
+enum CleanMask
+{
+    CLEAN_STORAGE           = 1,
+    CLEAN_SYSTEM_SETTINGS   = 2,
+    CLEAN_SETTINGS          = 4,
+    CLEAN_EXECUTABLE        = 8,
+};
+
+static void cleanup(CleanMask level_mask, const sw::SwContext &swctx)
+{
+    std::error_code ec;
+
+    if (level_mask & CLEAN_STORAGE)
+    {
+        // maybe add remove or cleanup method to Directories?
+        fs::remove_all(swctx.getLocalStorage().storage_dir);
+    }
+    if (level_mask & CLEAN_SYSTEM_SETTINGS)
+    {
+#ifdef _WIN32
+        // protocol handler
+        winreg::RegKey url(HKEY_CLASSES_ROOT);
+        url.TryDeleteTree(get_sw_registry_key());
+        // cmake
+        winreg::RegKey icon(HKEY_CURRENT_USER);
+        icon.TryDeleteTree(get_sw_cmake_registry_key()); // remove all empty cmake keys and trees?
+
+        // cygwin case
+        if (auto d = getenv("HOME"))
+        {
+            fs::remove(get_cmake_dir(d), ec);
+            fs::remove(get_cmake_dir(d) / get_sw_cmake_config_filename(), ec);
+        }
+#else
+        fs::remove(get_sw_linux_scheme_handler(), ec);
+        // cmake
+        fs::remove(get_cmake_dir(get_home_directory()), ec);
+        fs::remove(get_cmake_dir(get_home_directory()) / get_sw_cmake_config_filename(), ec);
+#endif
+    }
+    if (level_mask & CLEAN_SETTINGS)
+    {
+        fs::remove_all(sw::get_root_directory());
+    }
+    if (level_mask & CLEAN_EXECUTABLE)
+    {
+        auto prog = boost::dll::program_location().u8string();
+        String fn = prog;
+#ifdef _WIN32
+        fn = "timeout /t 3 && del \"" + fn + "\"";
+        const char *cmd = "cmd";
+        const char *args[] =
+        {
+            cmd,
+            "/c",
+            fn.c_str(),
+        };
+#else
+        const char *cmd = "rm";
+        const char *args[] =
+        {
+            cmd,
+            fn.c_str(),
+        };
+#endif
+        execve(cmd, args, 0);
+    }
+}
+
+static void cleanup(SwClientContext &swctx)
+{
+    int level = 0;
+    for (auto &l : swctx.getOptions().options_setup.level)
+        level |= l;
+    if (level == 0)
+        level = CLEAN_STORAGE | CLEAN_SYSTEM_SETTINGS;
+    cleanup((CleanMask)level, swctx.getContext(false));
+}
+
 SUBCOMMAND_DECL(setup)
 {
 #ifdef _WIN32
     elevate();
+#endif
 
+    if (getOptions().options_setup.uninstall)
+    {
+        cleanup(*this);
+        return;
+    }
+
+#ifdef _WIN32
     auto prog = boost::dll::program_location().wstring();
 
     // set common environment variable
@@ -75,7 +191,7 @@ SUBCOMMAND_DECL(setup)
 
     // set up protocol handler
     {
-        const std::wstring id = L"sw";
+        auto id = get_sw_registry_key();
 
         winreg::RegKey url(HKEY_CLASSES_ROOT, id);
         url.SetStringValue(L"URL Protocol", L"");
@@ -115,13 +231,11 @@ StartupNotify=false
 Terminal=true
 MimeType=x-scheme-handler/sw;
 )"s;
-    const auto fn = "sw-opener.desktop"s;
-    write_file(get_home_directory() / ".local/share/applications" / fn, opener);
+    write_file(get_sw_linux_scheme_handler(), opener);
     String cmd;
-    cmd = "xdg-mime default " + fn + " x-scheme-handler/sw";
+    cmd = "xdg-mime default " + get_sw_linux_scheme_handler().filename().string() + " x-scheme-handler/sw";
     if (system(cmd.c_str()))
         LOG_ERROR(logger, "Cannot register sw uri handler");
-
 #elif defined(__APPLE__)
 #endif
 
