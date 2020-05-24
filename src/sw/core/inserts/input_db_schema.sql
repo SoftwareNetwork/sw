eb0c3e36ccbaceec2df0e71721f59a420e967d1b
Egor Pugin <egor.pugin@gmail.com>
2020-05-24 22:11:59 +0300

    [core] Use SPDX identifiers for licenses.

--- src/sw/core/inserts/input_db_schema.sql
+++ src/sw/core/inserts/input_db_schema.sql
@@ -1,9 +1,6 @@
 --------------------------------------------------------------------------------
+-- SPDX-License-Identifier: GPL-3.0-or-later
 -- Copyright (C) 2020 Egor Pugin <egor.pugin@gmail.com>
---
--- This Source Code Form is subject to the terms of the Mozilla Public
--- License, v. 2.0. If a copy of the MPL was not distributed with this
--- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
