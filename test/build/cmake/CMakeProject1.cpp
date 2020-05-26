7ab64e438386118699b0f4ee9d929164d2debb8f
Egor Pugin <egor.pugin@gmail.com>
2020-05-26 03:03:26 +0300

    Add cmake fe test.

---
+++ test/build/cmake/CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
