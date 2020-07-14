ce6cdfab8e33b354f8e827f2b5064e7786ff8ab4
Egor Pugin <egor.pugin@gmail.com>
2020-07-14 14:35:52 +0300

    Change license for client library and tools to AGPLv3+.

--- src/sw/client/common/generator/vs/project_emitter.cpp
+++ src/sw/client/common/generator/vs/project_emitter.cpp
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2017-2019 Egor Pugin <egor.pugin@gmail.com>
 
 #include "project_emitter.h"
