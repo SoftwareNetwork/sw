c5e1b7f2b6e3be46e158e8555ae3c166f59502d6
Egor Pugin <egor.pugin@gmail.com>
2020-09-09 02:09:08 +0300

    Fix test.

--- test/build/cross/sw.cpp
+++ test/build/cross/sw.cpp
@@ -1,7 +1,7 @@
 void build(Solution &s)
 {
     auto &t = s.addExecutable("test");
-    t += cpp17;
+    t += cpp20;
     t += "main.cpp";
     t += "pub.egorpugin.primitives.sw.main-master"_dep;
 }
