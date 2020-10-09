57a7b5bc03e8beb6dddb37656a84ef4a64d87d29
Egor Pugin <egor.pugin@gmail.com>
2020-10-09 21:46:08 +0300

    [driver] Move builtin input into its own file.

--- src/sw/driver/build_self.cpp
+++ src/sw/driver/build_self.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2017-2020 Egor Pugin <egor.pugin@gmail.com>
 
+#include "builtin_input.h"
 #include "entry_point.h"
 
 #include <sw/core/input.h>
@@ -18,26 +19,6 @@ DECLARE_STATIC_LOGGER(logger, "build.self");
 #pragma warning(push)
 #pragma warning(disable : 4005) // warning C4005: 'XXX': macro redefinition
 #endif
-
-namespace sw
-{
-
-struct BuiltinInput : Input
-{
-    size_t h;
-
-    BuiltinInput(SwContext &swctx, const IDriver &d, size_t hash)
-        : Input(swctx, d, std::make_unique<Specification>(SpecificationFiles{})), h(hash)
-    {}
-
-    bool isParallelLoadable() const { return true; }
-    size_t getHash() const override { return h; }
-    EntryPointPtr load1(SwContext &) override { SW_UNREACHABLE; }
-};
-
-using BuiltinInputs = std::unordered_map<Input*, PackageIdSet>;
-
-}
 
 #include <build_self.generated.h>
 
---
+++ src/sw/driver/builtin_input.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2020 Egor Pugin <egor.pugin@gmail.com>
+
+#include <sw/core/input.h>
+
+namespace sw
+{
+
+struct BuiltinInput : Input
+{
+    size_t h;
+
+    BuiltinInput(SwContext &swctx, const IDriver &d, size_t hash)
+        : Input(swctx, d, std::make_unique<Specification>(SpecificationFiles{})), h(hash)
+    {}
+
+    bool isParallelLoadable() const { return true; }
+    size_t getHash() const override { return h; }
+    EntryPointPtr load1(SwContext &) override { SW_UNREACHABLE; }
+};
+
+using BuiltinInputs = std::unordered_map<Input*, PackageIdSet>;
+
+} // namespace sw
