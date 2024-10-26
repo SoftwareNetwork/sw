5d340bfc049860b737b187755c1caf1fd7ccaabf
Egor Pugin <egor.pugin@gmail.com>
2024-10-26 16:17:09 +0300

    Proper msvc check.

--- src/sw/driver/target/base.h
+++ src/sw/driver/target/base.h
@@ -167,7 +167,7 @@ struct SW_DRIVER_CPP_API TargetBase : TargetBaseData
     const Build &getSolution() const;
     const SwContext &getContext() const;
 
-#ifdef __cpp_explicit_this_parameter
+#if defined(_MSC_VER) && _MSC_VER >= 1932 || defined(__cpp_explicit_this_parameter)
     template <typename T>
     auto &set_loader(this T &obj, auto &&loader) {
         obj.loader = [f = loader](TargetBase &t) {
