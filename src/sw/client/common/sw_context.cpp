9e834bfb770ad0859c086d8cda9de2b8aa97beae
Egor Pugin <egor.pugin@gmail.com>
2024-11-30 11:22:24 +0300

    Set lib and link for gnu on windows.

--- src/sw/client/common/sw_context.cpp
+++ src/sw/client/common/sw_context.cpp
@@ -71,6 +71,8 @@ static sw::TargetSettings compilerTypeFromStringCaseI(const sw::UnresolvedPackag
         ts["native"]["program"]["c"] = set_with_version("org.gnu.gcc");
         ts["native"]["program"]["cpp"] = set_with_version("org.gnu.gpp");
         ts["native"]["program"]["asm"] = ts["native"]["program"]["c"].getValue();
+        ts["native"]["program"]["lib"] = set_with_version("org.gnu.binutils.ar");
+        ts["native"]["program"]["link"] = ts["native"]["program"]["cpp"].getValue();
     }
     else if (0
         || compiler.ppath == "clang"
